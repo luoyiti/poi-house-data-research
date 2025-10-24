d4362b913c2623bcc5571f0dd2074f9e013610b5
罗一逖 <luoyititi@icloud.com>
2025-10-24 10:34:16 +0800

    一点小改进

--- pages/数据分析.py
+++ pages/数据分析.py
@@ -9,10 +9,14 @@ st.markdown("## 住房与POI兴趣点分布地图")
 img = Image.open("./image/beijing_house_poi_map.png")
 st.image(img, caption="住房与POI兴趣点分布地图", use_container_width=True)
 
-st.markdown("## 北京房价分部地图")
+st.markdown("## 北京房价分布地图")
 # 显示图片
-img = Image.open("./image/beijing_house_price_map.png")
-st.image(img, caption="北京房价分部地图", use_container_width=True)
+img = Image.open("./image/beijing_house_price_map_with_basemap.png")
+st.image(img, caption="北京房价分布地图", use_container_width=True)
+
+st.markdown("## POI兴趣点邻近房价地图")
+img = Image.open("./image/beijing_poi_price_map.png")
+st.image(img, caption="POI兴趣点邻近房价地图", use_container_width=True)
 
 st.markdown("## 住房-POI兴趣点网络图")
 img = Image.open("./image/yeast_networkx.png")
