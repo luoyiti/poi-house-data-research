a05d07b119e1776054a2bcc16116df4c1719f107
罗一逖 <luoyititi@icloud.com>
2025-10-29 20:14:01 +0800

    增添了聚类分析部分，并且增添了模型预测的网页视图

---
+++ pages/数据展示.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+from PIL import Image
+
+# 从文件读取图片
+
+st.markdown("## 住房与POI兴趣点分布地图")
+img = Image.open("./image/beijing_house_poi_map.png")
+st.image(img, caption="住房与POI兴趣点分布地图", use_container_width=True)
+
+st.markdown("## 北京房价分布地图")
+# 显示图片
+img = Image.open("./image/beijing_house_price_map_with_basemap.png")
+st.image(img, caption="北京房价分布地图", use_container_width=True)
+
+st.markdown("## POI兴趣点邻近房价地图")
+img = Image.open("./image/beijing_poi_price_map.png")
+st.image(img, caption="POI兴趣点邻近房价地图", use_container_width=True)
+
+st.markdown("## 住房-POI兴趣点网络图")
+img = Image.open("./image/yeast_networkx.png")
+st.image(img, caption="总网络", use_container_width=True)
+img = Image.open("./image/subgraph_spring.png")
+st.image(img, caption="子网络(提取5000个节点)", use_container_width=True)
